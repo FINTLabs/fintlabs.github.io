ce396b45fcecdb0a6c705086f7b29c6c96aa8f90
Jarle Hansen <jarle@jarlehansen.net>
2017-04-26 12:43:34 +0200

    built hugo static resources

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 HUGO=`which hugo`
 if [ -f "$HUGO" ];
 then
-  hugo -d docs && git add . && git commit -m "built hugo static resources" && git push
+  hugo && git add . && git commit -m "built hugo static resources" && git push && git subtree push --prefix public origin master
 else
   echo 'Hugo is missing, install by running `brew install hugo`'
 fi
